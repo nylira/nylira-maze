13cfcd538b8e1298749af6a331c6e96d029c203b
Peng Zhong <peng@nylira.com>
2015-03-17 17:39:47 +0800

    create debug maze function

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ var DX       = { 4: 1, 8: -1, 1:  0, 2: 0 }
   , DY       = { 4: 0, 8:  0, 1: -1, 2: 1 }
   , OPPOSITE = { 4: W, W:  E, N:  S, S: N }
 
-
 //==============================================================================
 // variables
 
-var grid = new Grid(10, 10)
+// height, width
+var grid = new Grid(4, 4)
 
 //==============================================================================
 // functions
@@ -61,8 +61,61 @@ function carvePassagesFrom(cx, cy, grid) {
   }
 }
 
+function print(data) {
+  process.stdout.write(data)
+}
+
+function debugMaze(maze) {
+  for(var y=0; y < maze.length; y++) {
+    var slots = []
+    for(var x=0; x < maze[y].length; x++) {
+      var slot = ''
+      switch(maze[y][x]) {
+        case 0: slot  = '*ERR'; break
+        case 1: slot  = '   N'; break
+        case 2: slot  = '   S'; break
+        case 3: slot  = '  NS'; break
+        case 4: slot  = '   E'; break
+        case 5: slot  = '  NE'; break
+        case 6: slot  = '  SE'; break
+        case 7: slot  = '  NSE'; break
+        case 8: slot  = '   W'; break
+        case 9: slot  = '  NW'; break
+        case 10: slot = '  SW'; break
+        case 11: slot = ' NSW'; break
+        case 12: slot = '  EW'; break
+        case 13: slot = ' NEW'; break
+        case 14: slot = ' SEW'; break
+        case 15: slot = 'NSEW'; break
+      }
+      slots.push(slot)
+    }
+    console.log(slots)
+  }
+}
+
 // simple render function
 function renderMaze(maze) {
+  var grid = maze.data
+
+  // draw top border
+  console.log(' ' + '_'.repeat(maze.width * 2  - 1))
+
+  // draw each line
+  for(var y=0; y < maze.height; y++) {
+    print('|')
+
+    for(var x=0; x < maze.width; x++) {
+      print((grid[y][x] & S !== 0) ? ' ' : '_')
+      if(grid[y][x] & E !== 0) {
+        print(((grid[y][x] | grid[y][x+1]) & S !== 0) ? ' ' : '_')
+      } else {
+        print('|')
+      }
+    }
+    print('\n')
+  }
+
 }
 
 //==============================================================================
@@ -70,5 +123,8 @@ function renderMaze(maze) {
 
 carvePassagesFrom(0, 0, grid.data)
 console.log(grid.data)
+
+debugMaze(grid.data)
+renderMaze(grid)
 
 }())
